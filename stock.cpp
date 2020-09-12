e44e6941d858c3311b1697375f313233f5501bd0
Sarthak Aggarwal <sarthaakagarwal@gmail.com>
2020-09-12 17:15:53 +0530

    Add files via upload

---
+++ stock.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int profit(int prices[], int n)
+{
+    int pro = 0;
+
+    for(int i=1; i<n; i++)
+    {
+        if(prices[i] > prices[i-1])
+        {
+            pro += (prices[i]-prices[i-1]);
+        }
+    }
+    return pro;
+}
+
+
+int main()
+{
+
+    int n;
+    cin>>n;
+    int price[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>price[i];
+    }
+
+    cout<< profit(price,n) <<endl;
+
+    return 0;
+
+}
